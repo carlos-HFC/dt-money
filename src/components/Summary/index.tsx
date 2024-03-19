22f171b3a6f58328ffb4732ac7dfc0b6a795a957
Carlos Faustino <chfcchfc96@gmail.com>
2024-03-19 13:51:50 -0300

    feat: add summary hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -4,32 +4,13 @@ import {
   CurrencyDollar,
 } from "@phosphor-icons/react"
 
-import { useTransaction } from "../../contexts/Transactions"
+import { useSummary } from "../../hooks/useSummary"
 import { priceFormatter } from "../../utils/formatter"
 
 import { Container, SummaryCard } from "./style"
 
 export function Summary() {
-  const { transactions } = useTransaction()
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === "income") {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+  const summary = useSummary()
 
   return (
     <Container>
